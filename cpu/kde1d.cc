5de505e94dd83ed602deff7b28e2608cec29bd58
yli5 <yli5@caltech.edu>
2016-05-24 16:52:14 -0700

    add adaptive version in 1d

--- cpu/kde1d.cc
+++ cpu/kde1d.cc
@@ -2,11 +2,12 @@
 #include <math.h>
 #include <exception>
 #include <string>
+#include <iostream>
 #include "kde1d.h"
 
 #define PI 3.141592653
 
-kde1d::kde1d(double bandwidth_, std::vector<double> data_, std::string kernel_type_){
+kde1d::kde1d(double bandwidth_, std::vector<double> data_, std::string kernel_type_, bool adaptive_){
 	
 	if(data_.size() == 0){
 		throw std::domain_error("Sample data error: no sample data. ");
@@ -20,7 +21,11 @@ kde1d::kde1d(double bandwidth_, std::vector<double> data_, std::string kernel_ty
 
 	for(int i = 0; i < data_.size(); ++i) data.push_back(data_[i]);
 
+	// currently we support three kinds of kernel_types
 	kernel_type = kernel_type_;
+
+	// adaptive initialization
+	adaptive = adaptive_;
 }
 
 
@@ -37,10 +42,10 @@ void kde1d::add_data(double data_){
 }
 
 
-double kde1d::pdf(double x){
+double kde1d::pdf_naive(double x){
 	double result = 0;
 	if(data.size() == 0){
-		throw std::domain_error("Sample data error: no sample data for KDE. ");
+		throw std::domain_error("Sample data error: no sample data for naive KDE. ");
 	}
 
 	for(int i = 0; i < data.size(); ++i){
@@ -48,6 +53,56 @@ double kde1d::pdf(double x){
 	}
 	result = result / (bandwidth * data.size());
 	return result;
+}
+
+
+void kde1d::pilot_init(){
+	if(data.size() == 0){
+		throw std::domain_error("Sample data error: no sample data for adaptive KDE.");
+	}
+
+	// initialize pilot_density, which is to store pilot density estimate result.
+	for(int i = 0; i < data.size(); ++i){
+		pilot_density.push_back(pdf_naive(data[i]));
+	}
+
+	// initialize geometry_mean
+	for(int i = 0; i < pilot_density.size(); ++i){
+		geometry_mean *= pilot_density[i];
+	}
+	geometry_mean = pow(geometry_mean, 1.0/pilot_density.size());
+}
+
+
+
+double kde1d::pdf_adaptive(double x){
+	double result = 0;
+	if(data.size() == 0){
+		throw std::domain_error("Sample data error: no sample data for adaptive KDE.");
+	}
+
+	if(pilot_density.size() == 0){
+		pilot_init();
+		std::cout << "geometry_mean is " << geometry_mean << std::endl;
+		std::cout << "pilot_density0 is " << pilot_density[0] << std::endl;
+	}
+
+	double temp_bandwidth = 0;
+	for(int i = 0; i < data.size(); ++i){
+		temp_bandwidth = bandwidth * sqrt(geometry_mean/pilot_density[i]);
+		result += gauss_pdf((x - data[i]) / temp_bandwidth) / temp_bandwidth;
+	}
+	result = result / data.size();
+	return result;
+}
+
+double kde1d::pdf(double x){
+	if(adaptive == false){
+		return pdf_naive(x);
+	}
+	else{
+		return pdf_adaptive(x);
+	}
 }
 
 
--- cpu/kde1d.h
+++ cpu/kde1d.h
@@ -1,3 +1,6 @@
+// This class deals with one dimensional kernel density estimation.
+// It supports both naive and adaptive version, and provides three kernel types.
+
 #ifndef KERNEL_DENSITY1D_ESTIMATOR_H__
 #define KERNEL_DENSITY1D_ESTIMATOR_H__
 
@@ -7,15 +10,34 @@
 class kde1d{
 
 public:
+
+	// Constructor:
 	kde1d(){};
-	kde1d(double bandwidth, std::vector<double> data, std::string kernel_type_);
+
+	// The initialization should provides default bandwidth, 
+	// sample data, kernel type and which version(naive/adaptive). 
+	kde1d(double bandwidth, std::vector<double> data, 
+		std::string kernel_type_, bool adaptive_);
 	~kde1d() = default;
 
+	// These two functions are used to add more sample data points 
+	// when using this class.
 	void add_data(std::vector<double> data);
 	void add_data(double data);
 
+	// This function returns a naive version KDE
+	double pdf_naive(double x);
+
+	// This function returns adaptive KDE, in which the adaptive bandwidth
+	// is propotional to naive estimate in sample points.
+	double pdf_adaptive(double x);
+
+	// This function returns the estimated pdf of a given point. It will automatically
+	// calll pdf_naive or pdf_adaptive depending on user's need.
 	double pdf(double x);
 
+	// As an interface to bandwidth, these two functions can be used for cross validation
+	// to obtain optimal results.
 	double get_bandwidth();
 	void set_bandwidth(double bandwidth_);
 
@@ -24,6 +46,15 @@ private:
 	std::vector<double> data;
 	double gauss_pdf(double x);
 	std::string kernel_type;
+	
+	// related to adaptive
+	bool adaptive = false;
+
+	double geometry_mean = 1.0;
+	std::vector<double> pilot_density;
+	void pilot_init();
+
+
 
 };
 
